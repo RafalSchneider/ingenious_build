3c1fa53c877bfa6024e62a90c3dd6385731426ab
Rafal Schneider <rav86pl@hotmail.com>
2025-11-08 17:16:45 +0100

    first try

---
+++ app/Models/InvoiceProductLine.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class InvoiceProductLine extends Model
+{
+    protected $table = 'invoice_product_lines';
+    protected $fillable = [
+        'invoice_id',
+        'product_name',
+        'quantity',
+        'unit_price',
+        'total_unit_price',
+    ];
+
+    public function invoice()
+    {
+        return $this->belongsTo(Invoice::class);
+    }
+}
