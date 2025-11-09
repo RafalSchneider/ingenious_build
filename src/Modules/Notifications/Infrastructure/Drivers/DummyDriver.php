ac31e5ee35c04ea9f9b38b43acc67c0a73138e98
rschneid <rafal.schneider@capgemini.com>
2025-11-09 18:07:59 +0100

    Enhance DummyDriver to simulate email sending and trigger webhook callback

--- src/Modules/Notifications/Infrastructure/Drivers/DummyDriver.php
+++ src/Modules/Notifications/Infrastructure/Drivers/DummyDriver.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Modules\Notifications\Infrastructure\Drivers;
 
+use Illuminate\Support\Facades\Http;
+
 class DummyDriver implements DriverInterface
 {
     public function send(
@@ -12,6 +14,17 @@ class DummyDriver implements DriverInterface
         string $message,
         string $reference,
     ): bool {
+        // Dummy driver - symuluje wysłanie emaila
+        // W prawdziwej implementacji tutaj byłoby wywołanie API (SendGrid, Mailgun, etc.)
+
+        // Symuluj callback od dostawcy emaili - wywołaj webhook
+        $webhookUrl = route('notification.hook', [
+            'action' => 'delivered',
+            'reference' => $reference
+        ]);
+
+        Http::get($webhookUrl);
+
         return true;
     }
 }
